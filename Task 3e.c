dee399b407f35cc7816ed36ff76ad35d42161a82
nnm24ee127-TILAK <nnm24ee127@nmamit.in>
2025-11-17 23:14:50 +0530

    Implement delay in LED up-counter program
    Added a delay of 1 second in the counting loop to visualize the count on LEDs.

--- Task 3e.c
+++ Task 3e.c
@@ -9,10 +9,11 @@ unsigned char count;
 
 void main(void)
 {
-	P2=0x00;
+	P2=0x00;                             //set as output port
 	
 	for(count=0x00;count<=0xFF;count++)
 	{
-		P2=count;
+		P2=count;                        //can see upcounting value from 0x00 to 0xff in led array
+		delay(1000);                    //delay of 1s
 	}
 }
